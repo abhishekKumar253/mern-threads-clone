46826836335aba9f4b9fa4e95fc1d74fc1ef692f
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2024-07-14 23:30:26 +0530

    backend-update

---
+++ backend/models/postModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "User is required"],
+    },
+    text: {
+      type: String,
+      maxLength: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    comments: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        text: {
+          type: String,
+          required: true,
+        },
+        userProfilePic: {
+          type: String,
+        },
+        username: {
+          type: String,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
